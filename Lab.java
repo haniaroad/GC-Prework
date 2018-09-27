01e99ca2b4a695f24281d67511c8016371c191d0
Derien Stephens <haniaroadlifestyle@gmail.com>
2018-09-27 08:56:40 -0400

    Unit 1 Lab 1 Revisions

--- Lab.java
+++ Lab.java
@@ -2,26 +2,57 @@ package lab1;
 import java.util.Scanner;
 
 class Lab {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    final int MAX_LENGTH = 3; 
-    final int MIN_LENGTH = 3;
-    int num1 = 0;
+	  	public static void main(String[] args) {
+	  	
+	  		        int count = 0, userNum3;  //Get two 3 Digit numbers from user
+	  		        
+	  		        
+	  		         Scanner sc = new Scanner(System.in);
+	  		         System.out.println("Enter a 3 digit number: ");
+	  		         
+	  		                int userNum1 = sc.nextInt();
+	  		                
+	  		                 System.out.println("Enter a second 3 digit number:");
+	  		                 
+	  		                int userNum2 = sc.nextInt();
+	  		                userNum3 = userNum1;
+	  		               
+	  		                 while (userNum3 != 0) {
+	  		                 userNum3 = userNum3 / 10;
+	  		                 ++count;
+	  		                 }
+	  		                
+	  		                Myfunc(userNum1,userNum2,count);
+	  		                
+	  		    }
+	  		    
+	  		     public static void Myfunc(int a,int b,int n) {
+	  		         
+	  		          int outPut1,outPut2;
+	  		          int[] digit = new int[3];
+	  		          outPut1 = a;
+	  		          outPut2 =b;
+	  		        
+	  		     
+	  		     
+	  		       while ((a>0) & (b >0)) 
+	  		      {
+	  		        for(int i=0;i<n;i++) //Iterate through each number one by one
+	  		        {
+	  		        digit[i] = (a % 10)+(b%10);
+	  		        a /= 10; 
+	  		        b /= 10;
+	  		         }
+	  		        }
+	  		      //Validate that each numbers' place equals the following one   
+	  		      if((digit[0]== digit[1])&&(digit[1]==digit[2])&&(digit[2]==digit[0]))
+	  		      {
+	  		        System.out.println("True ");  
+	  		     }
+	  		      else
+	  		      {
+	  		         System.out.println("False");
+	  		       }
+	  		     }
+	  		}
 
-    System.out.print("Please enter a 3-digit number: ");
-    
-    scanner: //Validate
-    while(scanner.hasNext()) {
-      if(scanner.hasNextInt()){
-        num1 = scanner.nextInt();
-        if(String.valueOf(num1).length() < MAX_LENGTH) { //Limit to 3 digits
-        System.out.println("Not vailid entry. Please enter 3 digit number: ");
-        	scanner.next();
-        } 
-      } 
-       
-      
-    }
-    
-  }
-}
